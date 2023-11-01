28b59ef8b8929b3b4e3ef1189b298b1f09809b5e
Dima Osadchy <osadchyd05@gmail.com>
2023-11-01 20:17:16 +0200

    setup project

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@styles", replacement: "/src/styles" },
+      { find: "@app", replacement: "/src/app" },
+    ],
+  },
+});

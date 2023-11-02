88a52ab4c6761daba392ffafcb4cf193fcb2a212
Dima Osadchy <osadchyd05@gmail.com>
2023-11-02 17:12:11 +0200

    Created component of input

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Input } from "../common/components/Input";
+import { QuizButton } from "../common/components/QuizButton";
+import Icon from "../assets/icons/f_icon.svg";
+import Validate from "../assets/icons/validated_icon.svg";
+
 function App() {
-  return <></>;
+  const [name, setName] = useState();
+
+  const handleOnChange = (event) => setName(event.target.value);
+
+  return (
+    <>
+      <Input
+        placeholder="Введите имя"
+        onChange={handleOnChange}
+        label="Email"
+        id="name"
+        // endIcon={<img src={Validate} />}
+        errors={false}
+        value={name}
+      />
+      <QuizButton text="I'm man" icon={<img src={Icon} />} />
+    </>
+  );
 }
 
 export default App;
